87751e2807ea91d78a03ca867b7d71276dee68e7
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 18:02:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     genre_count 
   end 
 
-  def self.artist_count 
+  def song.artist_count 
     artist_count = Hash.new(0) 
     @@artists.each {|artist| artist_count[artist] += 1} 
     artist_count 
