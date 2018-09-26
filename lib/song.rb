41bc96793293d244740ce42e98d6bf826408c673
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 17:50:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,29 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  
+  @@count == 0
+
+  def initialize()
+
+  end
+
+  def count
+
+  end
+
+  def genres
+
+  end
+
+  def artists
+
+  end
+
+  def genre_count
+
+  end
+
+  def artist_count
+
+  end
 end
